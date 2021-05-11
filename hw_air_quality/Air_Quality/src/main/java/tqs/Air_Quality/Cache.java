e836e47ad8b4e2802b278db681ca72ea04675c6b
inespl <ines.pl@ua.pt>
2021-05-11 11:06:02 +0100

    hw-cache updated

--- hw_air_quality/Air_Quality/src/main/java/tqs/Air_Quality/Cache.java
+++ hw_air_quality/Air_Quality/src/main/java/tqs/Air_Quality/Cache.java
@@ -1,106 +1,63 @@
 package tqs.Air_Quality;
 
-import org.apache.commons.collections4.MapIterator;
-import org.apache.commons.collections4.map.LRUMap;
-import java.util.ArrayList;
+import tqs.entities.Quality;
 
-public class Cache<K, T> {
+import java.util.HashMap;
+import java.util.Map;
+
+public class Cache {
+    private Map cacheMap;
     private long timeToLive;
-    private LRUMap cacheMap;
 
-    protected class CacheObject {
+    public int hits;
+    public int misses;
+
+    public class CacheObject {
         public long lastAccessed = System.currentTimeMillis();
-        public T capacity;
+        public Quality quality;
 
-        protected CacheObject(T capacity){
-            this.capacity = capacity;
+        public CacheObject(Quality quality) {
+            this.quality = quality;
+        }
+        private Quality getQuality(){
+            return this.quality;
         }
     }
 
-    public Cache(long ttl, final long timerInterval, int capacity) {
-        this.timeToLive = ttl * 1000;
-        cacheMap = new LRUMap(capacity);
+    public Cache(long ttl) {
+        this.timeToLive = ttl;
+        cacheMap = new HashMap<String, CacheObject>();
 
-        if (timeToLive > 0 && timerInterval > 0) {
-            Thread t = new Thread(new Runnable() {
-                public void run() {
-                    while (true) {
-                        try {
-                            Thread.sleep(timerInterval * 1000);
-                        } catch (InterruptedException ex) {
-                        }
-                        cleanup();
-                    }
-                }
-            });
-
-            t.setDaemon(true);
-            t.start();
-        }
+        this.hits = 0;
+        this.misses = 0;
     }
 
-    public void put(K key, T value) {
-        synchronized (cacheMap) {
-            cacheMap.put(key, new CacheObject(value));
-        }
+    public void put(String key, Quality quality) {
+        cacheMap.put(key, new CacheObject(quality));
     }
 
-    @SuppressWarnings("unchecked")
-    public T get(K key) {
-        synchronized (cacheMap) {
-            CacheObject c = (CacheObject) cacheMap.get(key);
-
-            if (c == null)
-                return null;
-            else {
-                c.lastAccessed = System.currentTimeMillis();
-                return c.capacity;
-            }
-        }
-    }
-
-    public void remove(K key) {
-        synchronized (cacheMap) {
-            cacheMap.remove(key);
-        }
-    }
-
-    public int size() {
-        synchronized (cacheMap) {
-            return cacheMap.size();
-        }
-    }
-
-    @SuppressWarnings("unchecked")
-    public void cleanup() {
-
-        long now = System.currentTimeMillis();
-        ArrayList<K> deleteKey = null;
-
-        synchronized (cacheMap) {
-            MapIterator itr = cacheMap.mapIterator();
-
-            deleteKey = new ArrayList<K>((cacheMap.size() / 2) + 1);
-            K key = null;
-            CacheObject c = null;
-
-            while (itr.hasNext()) {
-                key = (K) itr.next();
-                c = (CacheObject) itr.getValue();
-
-                if (c != null && (now > (timeToLive + c.lastAccessed))) {
-                    deleteKey.add(key);
-                }
-            }
-        }
-
-        for (K key : deleteKey) {
-            synchronized (cacheMap) {
+    public Quality get(String key) {
+        if (cacheMap.containsKey(key)) {
+            CacheObject cO = (CacheObject) cacheMap.get(key);
+            if (cO.lastAccessed > timeToLive) {
+                misses += 1;
                 cacheMap.remove(key);
+            }else {
+                hits += 1;
+                return cO.getQuality();
             }
-
-            Thread.yield();
         }
+        return null;
     }
+
+    public int getHits() {
+        return hits;
+    }
+
+    public int getMisses() {
+        return misses;
+    }
+
 
 }
+
--- hw_air_quality/Air_Quality/src/test/java/tqs/Air_Quality/CacheTest.java
+++ hw_air_quality/Air_Quality/src/test/java/tqs/Air_Quality/CacheTest.java
@@ -1,83 +1,29 @@
 package tqs.Air_Quality;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CacheTest {
 
-    public static void main(String[] args) throws InterruptedException {
-
-        CacheTest crunchifyCache = new CacheTest();
-
-        System.out.println("\n\n==========Test1: crunchifyTestAddRemoveObjects ==========");
-        crunchifyCache.testAddRemoveObjects();
-        System.out.println("\n\n==========Test2: crunchifyTestExpiredCacheObjects ==========");
-        crunchifyCache.testExpiredCacheObjects();
-        System.out.println("\n\n==========Test3: crunchifyTestObjectsCleanupTime ==========");
-        crunchifyCache.testObjectsCleanupTime();
+    @BeforeEach
+    void setUp() {
     }
 
-    private void testAddRemoveObjects() {
-
-        // Test with crunchifyTimeToLive = 200 seconds
-        // crunchifyTimerInterval = 500 seconds
-        // maxItems = 6
-        Cache<String, String> cache = new Cache<String, String>(200, 500, 6);
-
-        cache.put("eBay", "eBay");
-        cache.put("Paypal", "Paypal");
-        cache.put("Google", "Google");
-        cache.put("Microsoft", "Microsoft");
-        cache.put("IBM", "IBM");
-        cache.put("Facebook", "Facebook");
-
-        System.out.println("6 Cache Object Added.. cache.size(): " + cache.size());
-        cache.remove("IBM");
-        System.out.println("One object removed.. cache.size(): " + cache.size());
-
-        cache.put("Twitter", "Twitter");
-        cache.put("SAP", "SAP");
-        System.out.println("Two objects Added but reached maxItems.. cache.size(): " + cache.size());
-
+    @Test
+    void testPut() {
     }
 
-    private void testExpiredCacheObjects() throws InterruptedException {
-
-        // Test with crunchifyTimeToLive = 1 second
-        // crunchifyTimerInterval = 1 second
-        // maxItems = 10
-        Cache<String, String> cache = new Cache<String, String>(1, 1, 10);
-
-        cache.put("eBay", "eBay");
-        cache.put("Paypal", "Paypal");
-        // Adding 3 seconds sleep.. Both above objects will be removed from
-        // Cache because of timeToLiveInSeconds value
-        Thread.sleep(3000);
-
-        System.out.println("Two objects are added but reached timeToLive. cache.size(): " + cache.size());
-
+    @Test
+    void testGet() {
     }
 
-    private void testObjectsCleanupTime() throws InterruptedException {
-        int size = 500000;
+    @Test
+    void testGetHits() {
+    }
 
-        // Test with timeToLiveInSeconds = 100 seconds
-        // timerIntervalInSeconds = 100 seconds
-        // maxItems = 500000
-
-        Cache<String, String> cache = new Cache<String, String>(100, 100, 500000);
-
-        for (int i = 0; i < size; i++) {
-            String value = Integer.toString(i);
-            cache.put(value, value);
-        }
-
-        Thread.sleep(200);
-
-        long start = System.currentTimeMillis();
-        cache.cleanup();
-        double finish = (double) (System.currentTimeMillis() - start) / 1000.0;
-
-        System.out.println("Cleanup times for " + size + " objects are " + finish + " s");
-
+    @Test
+    void testGetMisses() {
     }
 }
